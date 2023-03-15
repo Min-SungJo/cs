7c1e4a308120b41c34a050fe6aeb78a304502698
Min-SungJo <123or@naver.com>
2023-03-16 00:35:10 +0900

    repeat *을 n개 출력, w개 마다 줄바꿈

---
+++ src/main/java/org/example/programmers/org/example/algorithmDataStructure/Repeat.java
@@ -0,0 +1,40 @@
+package org.example.programmers.org.example.algorithmDataStructure;
+
+import java.util.Scanner;
+
+/**
+ * repeat 은 String 클래스에 있는 인스턴스 메서드임
+ * 문자열.repeat(숫자)
+ * 문자열을 숫자만큼 반복하여 생성, 반환
+ * */
+public class Repeat {
+    /**
+     "*" 을 n개 출력, w개 마다 줄바꿈
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("n값: ");
+        int n = scan.nextInt();
+        System.out.print("w값: ");
+        int w = scan.nextInt();
+        System.out.println();
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("*");
+            if ((i + 1) % w == 0) {
+                System.out.println();
+            }
+
+        }
+        System.out.println();
+        System.out.println();
+        for (int i = 0; i < n / w; i++) {
+            System.out.println("*".repeat(w));
+        }
+
+        int rest = n % w;
+        if (rest != 0) {
+            System.out.println("*".repeat(rest));
+        }
+    }
+}
