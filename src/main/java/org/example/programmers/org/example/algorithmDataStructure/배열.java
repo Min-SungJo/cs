fbb6733c0a7a0ade8b5dc048702e44a13193d1d6
Min-SungJo <123or@naver.com>
2023-03-17 20:31:03 +0900

    소수 구하기 (배열 활용에 따른 연산 수 차이)

--- src/main/java/org/example/programmers/org/example/algorithmDataStructure/배열.java
+++ src/main/java/org/example/programmers/org/example/algorithmDataStructure/배열.java
@@ -1,6 +1,7 @@
 package org.example.programmers.org.example.algorithmDataStructure;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * 같은 자료형의 변수인 구성 요소(component)가 모인 것
@@ -17,7 +18,6 @@ public class 배열 {
 
         /**요소값을 초기화 하면서 배열을 선언*/
         int[] arrInt = {1, 2, 3, 5, 4};
-
         StringBuilder arr = new StringBuilder();
         System.out.println(arr.append("배열: ").append(Arrays.toString(arrInt)));
 
@@ -30,6 +30,12 @@ public class 배열 {
 
         StringBuilder sum = new StringBuilder();
         System.out.println(sum.append("총합: ").append(maxOf(arrInt)));
+
+        System.out.print("숫자 입력: ");
+        Scanner scan = new Scanner(System.in);
+        int inputNum = scan.nextInt();
+        primeNum1(inputNum);
+        primeNum2(inputNum);
     }
 
     /**
@@ -51,24 +57,79 @@ public class 배열 {
         int t = 0;
         for (int i = 0; i < arr.length / 2; i++) {
             t = arr[i];
-            arr[i] = arr[arr.length-(i+1)];
-            arr[arr.length-(i+1)] = t;
+            arr[i] = arr[arr.length - (i + 1)];
+            arr[arr.length - (i + 1)] = t;
         }
         return arr;
     }
 
     /**
      * 배열의 모든 요소의 합계
-     * */
+     */
     public static int sumOf(int[] arr) {
         if (arr.length == 1) {
             return arr[0];
-        }else {
+        } else {
             int sum = 0;
             for (int i : arr) {
                 sum += i;
             }
             return sum;
+        }
+    }
+
+    /**
+     * 2부터, 입력한 숫자까지 소수 출력1
+     */
+    public static void primeNum1(int num) {
+        int totalPrimeNum = 1;
+        int counter = 0;
+        if (num < 2) {
+            System.out.println("1보다 큰 숫자를 입력하시오");
+        } else if (num >= 2) {
+            for (int i = 3; i <= num; i += 2) {
+                for (int j = 2; j <= i; j++) {
+                    counter++;
+                    if (j == i) {
+//                        System.out.println(j);
+                        totalPrimeNum++;
+                    } else {
+                        if (i % j == 0) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("소수 수: " + totalPrimeNum);
+        System.out.println("연산수: " + counter); // 77690
+    }
+
+    /**
+     * 2부터, 입력한 숫자까지의 소수 출력(배열 활용)2
+     */
+    public static void primeNum2(int num) {
+        // n 이 소수인지 확인하기 위해선 n 이하의 소수를 나누어서 확인하는 것만으로 충분하다
+        if (num >= 2) {
+            int counter = 0;
+            int ptr = 0;
+            int[] prime = new int[num / 2 + 1];
+            prime[ptr++] = 2;
+
+            for (int n = 3; n<=num; n+=2) {
+                int i;
+                for (i = 1; i<ptr; i++) { // 배열에 있는 값으로 연산할 예정
+                    counter++;
+                    if ( n % prime[i]==0) {
+                        break;
+                    }
+                }
+                if ( ptr == i ) {
+                    prime[ptr++] = n;
+                }
+            }
+            System.out.println("소수 수: "+ptr);
+            System.out.println("연산수: "+counter); // 14622
         }
     }
 }
