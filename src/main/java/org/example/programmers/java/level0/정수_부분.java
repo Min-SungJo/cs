fd27b72aeba2ad380eaf2fcbb44773b7e96e5b0a
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-05-18 01:30:28 +0900

    프로그래머스 level 0 정수 부분

---
+++ src/main/java/org/example/programmers/java/level0/정수_부분.java
@@ -0,0 +1,13 @@
+package org.example.programmers.java.level0;
+
+public class 정수_부분 {
+    public static void main(String[] args) {
+        double flo = 69.32;
+        int result = solution(flo);
+        System.out.println(result);
+    }
+    public static int solution(double flo) {
+        return (int)flo;
+
+    }
+}
