b588524b626f9a16db0781a849bf4809a9d220f7
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-05-19 03:22:23 +0900

    프로그래머스 level 0 대문자로 바꾸기

---
+++ src/main/java/org/example/programmers/java/level0/대문자로_바꾸기.java
@@ -0,0 +1,14 @@
+package org.example.programmers.java.level0;
+
+public class 대문자로_바꾸기 {
+    public static void main(String[] args) {
+
+        String str = "aBcDeFg";
+        String result = solution(str);
+        System.out.println(result);
+    }
+
+    private static String solution(String str) {
+        return str.toUpperCase();
+    }
+}
