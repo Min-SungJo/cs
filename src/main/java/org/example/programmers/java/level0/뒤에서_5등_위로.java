1ef00e9d239720c74b200d806a18950c011957a7
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-05-23 02:16:40 +0900

    프로그래머스 level 0 뒤에서 5등 위로

---
+++ src/main/java/org/example/programmers/java/level0/뒤에서_5등_위로.java
@@ -0,0 +1,23 @@
+package org.example.programmers.java.level0;
+
+import java.util.Arrays;
+
+public class 뒤에서_5등_위로 {
+    public static void main(String[] args) {
+
+        int[] num_list = {12, 4, 15, 46, 38, 1, 14, 56, 32, 10};
+        int[] result = solution(num_list);
+        System.out.println(result[0]);
+        System.out.println(result[1]);
+        System.out.println(result[2]);
+    }
+
+    private static int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        int[] sort_list = new int[num_list.length-5];
+        for(int i = 0; i < sort_list.length; i++) {
+            sort_list[i] = num_list[5+i];
+        }
+        return sort_list;
+    }
+}
