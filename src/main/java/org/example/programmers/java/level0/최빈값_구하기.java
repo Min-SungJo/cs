08afb2dfd20ae03139f7bb7d506627e7ca009745
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-05-29 19:17:19 +0900

    프로그래머스 level 0 최빈값 구하기 Map 사용

--- src/main/java/org/example/programmers/java/level0/최빈값_구하기.java
+++ src/main/java/org/example/programmers/java/level0/최빈값_구하기.java
@@ -1,41 +1,32 @@
 package org.example.programmers.java.level0;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class 최빈값_구하기 {
     public static void main(String[] args) {
-        int[] array = {2};
+        int[] array = {1};
         int result = solution(array);
         System.out.println(result);
     }
 
-    private static int solution(int[] array) {
-        Arrays.sort(array);
+    public static int solution(int[] array) {
 
-        int key = array[0];
-        int count = 1;
+        Map<Integer, Integer> map = new HashMap<>();
         int mode = array[0];
-        int modeCount = 1;
-        boolean change = false;
-        if (array.length == 1) {
-            return key;
-        }
-        for(int i =1; i < array.length; i++) {
-            if(array[i]==key) {
-                count++;
-                if(count ==modeCount) {
-                    mode = -1;
-                } else if (count > modeCount) {
-                    change = true;
-                    modeCount = count;
-                    mode = key;
-                }
-            } else{
-                count = 1;
-                key = array[i];
+        int modeCount = 0;
+        for(int number : array) {
+            int count = map.getOrDefault(number,0)+1;
+            System.out.println(count);
+            System.out.println(modeCount);
+            if(count > modeCount) {
+                mode = number;
+                modeCount = count;
+            } else if (count == modeCount) {
+                mode = -1;
             }
+            map.put(number,count);
         }
-        return change?mode:-1;
+        return mode;
     }
 }
