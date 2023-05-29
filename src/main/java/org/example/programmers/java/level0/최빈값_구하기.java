74f4b036d7f1d5138a0b8cd4595572f7a00d6b02
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-05-29 16:50:45 +0900

    프로그래머스 level 0 최빈값 구하기

---
+++ src/main/java/org/example/programmers/java/level0/최빈값_구하기.java
@@ -0,0 +1,39 @@
+package org.example.programmers.java.level0;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class 최빈값_구하기 {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4};
+        int result = solution(array);
+        System.out.println(result);
+    }
+
+    private static int solution(int[] array) {
+        Arrays.sort(array);
+
+        int key = array[0];
+        int count = 1;
+        int mode = array[0];
+        int modeCount = 1;
+        boolean change = false;
+
+        for(int i =1; i < array.length; i++) {
+            if(array[i]==key) {
+                count++;
+                if(count ==modeCount) {
+                    mode = -1;
+                } else if (count > modeCount) {
+                    change = true;
+                    modeCount = count;
+                    mode = key;
+                }
+            } else{
+                count = 1;
+                key = array[i];
+            }
+        }
+        return change?mode:-1;
+    }
+}
