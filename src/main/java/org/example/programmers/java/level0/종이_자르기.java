c5e394d4d1282dbf612cab0c977fcf1742342e32
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-06-23 21:57:14 +0900

    프로그래머스 level 0 종이 자르기

---
+++ src/main/java/org/example/programmers/java/level0/종이_자르기.java
@@ -0,0 +1,13 @@
+package org.example.programmers.java.level0;
+
+public class 종이_자르기 {
+    public static void main(String[] args) {
+        int N = 2;
+        int M = 5;
+        System.out.println(solution(N,M));
+    }
+    public static int solution(int N, int M) {
+        return N*M-1;
+
+    }
+}
