5dbab39f4d3237b306937561f46eba52757ba770
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-07-16 23:41:53 +0900

    프로그래머스 level 1 콜라 문제

--- src/main/java/org/example/programmers/java/level1/콜라_문제.java
+++ src/main/java/org/example/programmers/java/level1/콜라_문제.java
@@ -10,17 +10,17 @@ public class 콜라_문제 {
     }
 
     public static int solution(int a, int b, int n) {
-        int pay = a;
-        int payback = b;
-        int total = n;
-        int answer = 0;
+        int pay = a; // 3
+        int payback = b; // 2
+        int total = n; // 10
+        int answer = 0; // 16
 //        while (n >= a) {
 //            int num = n / a * b;
 //            n %= a;
 //            answer += num;
 //            n += num;
 //        }
-        if (total <= payback) return 0;
+//        if (total <= payback) return 0;
         answer = (total - payback) / (pay - payback) * payback;
         return answer;
     }
