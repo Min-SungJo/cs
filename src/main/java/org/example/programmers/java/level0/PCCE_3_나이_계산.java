7fb5c78905a9c299be826e35513c7e8252ec8882
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-12-27 19:27:33 +0900

    프로그래머스 java level 0 [PCCE 기출문제] 3번 / 나이 계산

---
+++ src/main/java/org/example/programmers/java/level0/PCCE_3_나이_계산.java
@@ -0,0 +1,20 @@
+package org.example.programmers.java.level0;
+
+import java.util.Scanner;
+
+public class PCCE_3_나이_계산 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+        String age_type = sc.next();
+        int answer = 0;
+
+        if (age_type.equals("Korea")) {
+            answer = 2030 - year + 1;
+        } else if (age_type.equals("Year")) {
+            answer = 2030 - year;
+        }
+
+        System.out.println(answer);
+    }
+}
