32e7aea79b2430ca2d28dde63ea8f429e3545506
민성조 <91239439+Min-SungJo@users.noreply.github.com>
2023-12-27 19:37:23 +0900

    프로그래머스 java level 0 [PCCE 기출문제] 5번 / 나이 계산

---
+++ src/main/java/org/example/programmers/java/level0/PCCE_5_산책.java
@@ -0,0 +1,29 @@
+package org.example.programmers.java.level0;
+
+
+public class PCCE_5_산책 {
+    public int[] solution(String route) {
+        int east = 0;
+        int north = 0;
+        int[] answer = new int[2];
+        for (int i = 0; i < route.length(); i++) {
+            switch (route.charAt(i)) {
+                case 'N':
+                    north++;
+                    break;
+                case 'S':
+                    north--;
+                    break;
+                case 'E':
+                    east++;
+                    break;
+                case 'W':
+                    east--;
+                    break;
+            }
+        }
+        answer[0] = east;
+        answer[1] = north;
+        return answer;
+    }
+}
